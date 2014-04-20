8cd47aeb4a5a5a8279c24b707ee0bb49af64d2d6
crichon <github@crichon.eu>
2014-04-20 15:27:23 +0200

    test

--- tx/settings.py
+++ tx/settings.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__)).replace('\\','/')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 root = os.path.dirname(os.path.dirname(__file__)).replace('\\','/')
 MEDIA_ROOT = root + '/../media/'
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'static'
+STATIC_URL = '/static/'
 STATIC_DIRS = (os.path.join(BASE_DIR, u'static'),)
 TEMPLATE_DIRS = (
         root + '/../templates'
