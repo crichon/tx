cad7ac3e52276b4bf360965a4079c3c5dae9553e
crichon <github@crichon.eu>
2014-04-23 15:03:14 +0200

    settings heroku

--- tx/settings.py
+++ tx/settings.py
@@ -68,13 +68,16 @@ WSGI_APPLICATION = 'tx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'daqkph7qnc6bhv',
+    'HOST': 'ec2-54-204-24-154.compute-1.amazonaws.com',
+    'PORT': 5432,
+    'USER': 'nemfikadudwjsz',
+    'PASSWORD': 'Gr9gqlwARMacBLpeo3nPhbe8Fz'
+  }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
