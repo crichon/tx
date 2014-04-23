80b80d391bdf3539b5e8fd053fc898834ccae6bf
Matthieu Hanne <matthieu.hanne@gmail.com>
2014-04-23 13:25:45 +0200

    Let's go

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from main.models import Item, Supplier
 
+def index(request):
+	return HttpResponse("Commande courrante ?")
+
+def toolbox(request):
+	return HttpResponse("Bienvenu dans la toolbox")
 # Create your views here.
+
+def importItem(xlsFile):
+
+
+
--- tx/urls.py
+++ tx/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
+from main import views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tx.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^toolbox/', views.xls, name='toolbox'),
     url(r'^admin/', include(admin.site.urls)),
 )
