1cea59926cb6d6314ea3d2478f8dd403e922c9ad
crichon <github@crichon.eu>
2014-07-28 21:52:10 +0200

    before deployement

--- gestion_produits/views.py
+++ gestion_produits/views.py
@@ -15,23 +15,24 @@ def toolbox(request):
 	return HttpResponse("Initialisation terminee")
 
 def importItem(file_path):
+    """ Initialize product table from an xls file"""
 
-	#Ouverture du fichier
-	rb = open_workbook(file_path)
-	r_sheet = rb.sheet_by_index(0)
+    #Ouverture du fichier
+    rb = open_workbook(file_path)
+    r_sheet = rb.sheet_by_index(0)
 
-	for row_index in range (1, r_sheet.nrows):
-		#Hydratation or get Supplier Model
-		item_supplier= r_sheet.cell(row_index, 4).value
-		item_supplier, created = Supplier.objects.get_or_create(name=item_supplier)
+    for row_index in range (1, r_sheet.nrows):
+            #Hydratation or get Supplier Model
+            item_supplier= r_sheet.cell(row_index, 4).value
+            item_supplier, created = Supplier.objects.get_or_create(name=item_supplier)
 
-		#Hydratation or get Category Model
-		current_category =  r_sheet.cell(row_index, 0).value
-		item_category, created = Category.objects.get_or_create(name=current_category)
+            #Hydratation or get Category Model
+            current_category =  r_sheet.cell(row_index, 0).value
+            item_category, created = Category.objects.get_or_create(name=current_category)
 
-		#Hydratation Item
-		item_name = r_sheet.cell(row_index, 1).value
-		item_ref = current_supplier= r_sheet.cell(row_index, 3).value
-		item, created = Item.objects.get_or_create(ref=item_ref, name=item_name, category=item_category, supplier=item_supplier)
+            #Hydratation Item
+            item_name = r_sheet.cell(row_index, 1).value
+            item_ref = current_supplier= r_sheet.cell(row_index, 3).value
+            item, created = Item.objects.get_or_create(ref=item_ref, name=item_name, category=item_category, supplier=item_supplier)
 
 
